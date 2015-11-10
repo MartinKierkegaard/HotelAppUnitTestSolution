eaf46644eb2930d0744aed3e058d950d67cc3eb3
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2015-11-10 22:54:51 +0100

    added unittest and consoleapp

---
+++ HotelAppUnitTest/UnitTestRoom.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotelFlatFilesApp;
+
+namespace HotelAppUnitTest
+{
+    [TestClass]
+    public class UnitTestRoom
+    {
+        [TestMethod]
+        public void ParseOkTest()
+        {
+            string roomText = "100001200340";
+            var room = new Room();
+
+            bool ok = room.TryParse(roomText);
+
+            Assert.IsTrue(ok);
+        }
+
+        [TestMethod]
+        public void ParseOkTestHotelNo()
+        {
+            string roomText = "100001200340";
+            var room = new Room();
+            bool ok = room.TryParse(roomText);
+            Assert.AreEqual(room.HotelNo, 1);
+        }
+
+
+        [TestMethod]
+        public void TestIfLengthGt12()
+        {
+            string roomText = "1100001200340";
+            var room = new Room();
+
+            bool ok = room.TryParse(roomText);
+            //Assert.AreEqual(false, ok);
+            Assert.IsFalse(ok);
+        }
+
+        [TestMethod]
+        public void ParseNotOkTest()
+        {
+            string roomText = "a00001200340";
+            var room = new Room();
+
+            bool ok = room.TryParse(roomText);
+
+            Assert.AreEqual(false, ok);
+        }
+
+        [TestMethod]
+        public void ParseNotOkTestHotelNoInvalid()
+        {
+            string roomText = "a00001200340";
+            var room = new Room();
+
+            bool ok = room.TryParse(roomText);
+
+            Assert.AreEqual(0,room.HotelNo);
+        }
+
+
+    }
+}
---
+++ HotelFlatFilesApp/Room.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFlatFilesApp
+{
+    public class Room
+    {
+        //Record layout
+        //1-3 RoomNo
+        //4-6 Hotelno
+        //7-7 RoomType
+        //8-12 Price
+
+        public Room()
+        {
+
+        }
+
+        public int RoomNo { get; set; }
+        public int HotelNo { get; set; }
+        public RoomType Types { get; set; }
+        public int Price { get; set; }
+
+
+        //public static bool TryParse(string input, out Room room)
+        //{
+        //    var myParsedRoom = new Room();
+
+        //    room = null;
+        //    string roomNo = input.Substring(1, 3);
+        //    string hotelNo = input.Substring(4, 3);
+        //    string roomType = input.Substring(7, 1);
+        //    string price = input.Substring(8, 5);
+
+        //    int roomNoResult;
+        //    bool roomOk = Int32.TryParse(roomNo, out roomNoResult);
+
+        //    int hotelNoResult;
+        //    bool hotelNoOk = Int32.TryParse(hotelNo, out hotelNoResult);
+
+        //    RoomType roomTypeResult;
+        //    bool roomTypeOk = Enum.TryParse(roomType, out roomTypeResult);
+
+        //    int priceResult;
+        //    bool priceOK = Int32.TryParse(price, out priceResult);
+
+        //    bool isParsedOk = roomOk || hotelNoOk || roomTypeOk || priceOK;
+
+        //    if (isParsedOk)
+        //    {
+        //        myParsedRoom.RoomNo = roomNoResult;
+        //        myParsedRoom.HotelNo = hotelNoResult;
+        //        myParsedRoom.Types = roomTypeResult;
+        //        myParsedRoom.Price = priceResult;
+        //    }
+        //    room = myParsedRoom;
+
+        //    return isParsedOk;
+        //}
+
+        /// <summary>
+        /// a method that takes a string of length 12
+        /// and parse it to a Room object
+        /// Record layout for Room
+        ///  1-3 RoomNo
+        ///  4-6 Hotelno
+        ///  7-7 RoomType
+        ///  8-12 Price
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public bool TryParse(string input)
+        {
+            Console.WriteLine("input: "+ input);
+            Console.WriteLine("Length: "+ input.Length);
+           
+            var myParsedRoom = new Room();
+
+            string roomNo = input.Substring(0, 3);
+            string hotelNo = input.Substring(2, 3);
+            string roomType = input.Substring(6, 1);
+            string price = input.Substring(6, 5);
+
+            int roomNoResult;
+            bool roomOk = Int32.TryParse(roomNo, out roomNoResult);
+
+            int hotelNoResult;
+            bool hotelNoOk = Int32.TryParse(hotelNo, out hotelNoResult);
+
+            RoomType roomTypeResult;
+            bool roomTypeOk = Enum.TryParse(roomType, out roomTypeResult);
+
+            int priceResult;
+            bool priceOK = Int32.TryParse(price, out priceResult);
+            // bool priceOK = Int32.TryParse(price28, out priceResult28);
+            priceResult += 28;//is that really necessary ??
+
+            bool isParsedOk = roomOk || hotelNoOk || roomTypeOk || priceOK;
+
+            if (isParsedOk)
+            {
+                this.RoomNo = roomNoResult;
+                this.HotelNo = hotelNoResult;
+                this.Types = roomTypeResult;
+                this.Price = priceResult;
+            }
+
+            return isParsedOk;
+        }
+
+
+    }
+
+    public enum RoomType
+    {
+        Single = 1,
+        Double = 2,
+        Family = 3
+    }
+}
